afbf570e7ca590fb91771b0761ecedf2cc6b647d
Kartikey Varshney <kartikeyvarshney8@gmail.com>
2026-06-30 15:09:29 +0530

    Configure production CORS

--- server/app.js
+++ server/app.js
@@ -9,7 +9,14 @@ const app = express();
 const interviewRoutes = require("./routes/interviewRoutes");
 const reportRoutes = require("./routes/reportRoutes");
 
-app.use(cors());
+app.use(
+  cors({
+    origin:
+      process.env.CLIENT_URL ||
+      "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
 
 app.use(
