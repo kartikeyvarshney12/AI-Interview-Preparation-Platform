6a381b0a389a24c636b6ea73144e9b0f34a592c4
Kartikey Varshney <kartikeyvarshney8@gmail.com>
2026-06-30 15:39:16 +0530

    Configure frontend environment variables

--- client/src/services/interviewService.js
+++ client/src/services/interviewService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api/interviews";
+const API_URL = `${import.meta.env.VITE_API_URL}/interviews`;
 
 const handleApiError = (error) => {
   if (error.response?.status === 401) {
