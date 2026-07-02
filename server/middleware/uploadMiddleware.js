6f016ccfe6fe8de3c9d53d533424e4de6aea25e3
Kartikey Varshney <kartikeyvarshney8@gmail.com>
2026-07-02 08:58:40 +0530

    Create uploads directory automatically

--- server/middleware/uploadMiddleware.js
+++ server/middleware/uploadMiddleware.js
@@ -1,16 +1,20 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
+
+const uploadDir = path.join(__dirname, "../uploads");
+
+if (!fs.existsSync(uploadDir)) {
+  fs.mkdirSync(uploadDir, { recursive: true });
+}
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, "uploads/");
+    cb(null, uploadDir);
   },
 
   filename: (req, file, cb) => {
-    cb(
-      null,
-      Date.now() + path.extname(file.originalname)
-    );
+    cb(null, Date.now() + path.extname(file.originalname));
   },
 });
 
